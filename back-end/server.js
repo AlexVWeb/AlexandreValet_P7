610addad4768ea188aa76b36bab51423b7d4d0fa
AlexVWeb <alex-v00@hotmail.fr>
2021-02-21 10:47:11 +0100

    BE: ajout de socketIo

--- back-end/server.js
+++ back-end/server.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const app = require('./app');
+const Message = require("./models/Message");
 
 const normalizePort = val => {
     const port = parseInt(val, 10);
@@ -37,11 +38,40 @@ const errorHandler = error => {
 
 const server = http.createServer(app);
 
-server.on('error', errorHandler);
-server.on('listening', () => {
-    const address = server.address();
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-    console.log('Listening on ' + bind);
+
+const io = require('socket.io')(server, {
+    cors: {
+        origin: process.env.APP_URL,
+        methods: ["GET", "POST"],
+        credentials: true
+    },
+    allowEIO3: true
+});
+let interval;
+
+io.on("connection", (socket) => {
+    // console.log("New client connected");
+    if (interval) {
+        clearInterval(interval);
+    }
+
+    socket.on("message", async (message) => {
+        await (new Message()).createTable()
+        console.log(message)
+        let insert = (new Message()).insert({
+            userID: message.user.id,
+            date: message.date,
+            content: message.content
+        })
+        console.log(insert)
+        socket.broadcast.emit("newMessage", message);
+    });
+
+    socket.on("disconnect", () => {
+        // console.log("Client disconnected");
+        clearInterval(interval);
+    });
 });
 
-server.listen(port);
+server.on('error', errorHandler);
+server.listen(port, () => console.log(`Listening on port ${port}`));
