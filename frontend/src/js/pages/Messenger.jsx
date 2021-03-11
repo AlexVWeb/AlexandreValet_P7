623645907718e1cf77e915564e90d2c8bc8cae33
AlexVWeb <alex-v00@hotmail.fr>
2021-03-11 10:27:19 +0100

    FE: Update Messenger.jsx setting FileModal

--- frontend/src/js/pages/Messenger.jsx
+++ frontend/src/js/pages/Messenger.jsx
@@ -2,9 +2,12 @@ import React, {useEffect, useState} from "react"
 import Sidebar from "../modules/Sidebar";
 import Message from "../modules/Message";
 import socketIOClient from "socket.io-client";
-import User from "../controllers/user"
 import moment from 'moment';
 import {ModalProfil} from "../modules/ModalProfil";
+import UserController from "../Class/User";
+import FileInput from "../modules/FileInput";
+import {Modal} from "../modules/Modal";
+import FileModal from "../modules/FileModal";
 
 moment.locale('fr')
 
@@ -14,9 +17,12 @@ const io = socketIOClient(ENDPOINT, {
 })
 
 export default function Messenger() {
-    let user = (new User()).getCurrentUser()
+    let user = UserController.getCurrentUser()
+
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([])
+    const [messageFile, setMessageFile] = useState(null)
+    const [temporyFile, setTemporyFile] = useState(null)
 
     const scrollToBottom = (node) => {
         node.scrollTop = node.scrollHeight;
@@ -85,9 +91,55 @@ export default function Messenger() {
         }
     }
 
+
+    if (messageFile) {
+        let newMessage = {
+            date: moment().format("YYYY-MM-DD HH:mm:ss"),
+            user: {
+                id: user.userId,
+                pseudo: user.pseudo
+            }
+        }
+
+        if (messageFile.message !== undefined) {
+            newMessage = {...newMessage, content: messageFile.message}
+        }
+
+        const fileReader = new FileReader(),
+            slice = messageFile.file.slice(0, 100000);
+
+        fileReader.readAsArrayBuffer(slice);
+        fileReader.onload = () => {
+            const arrayBuffer = fileReader.result;
+            newMessage = {
+                ...newMessage,
+                name: messageFile.file.name,
+                type: messageFile.file.type,
+                size: messageFile.file.size,
+                data: arrayBuffer}
+
+            io.emit('message.image', newMessage);
+        }
+    }
+
+    let modalFile = document.querySelector('#fileModal')
+    if (modalFile) {
+        modalFile.addEventListener('hidden.bs.modal', function () {
+            setTemporyFile(null)
+            setMessageFile(null)
+        })
+    }
+
     return <>
         <div id={"messenger"} className={"container-fluid container_messenger"}>
-            <ModalProfil/>
+            <Modal id={'modalAccount'} title="Éditer mon profil"><ModalProfil/></Modal>
+
+            <Modal id={'fileModal'}>
+                {
+                    temporyFile && <FileModal fileObject={temporyFile} messageFile={(data) => setMessageFile(data)}/>
+                }
+            </Modal>
+
             <Sidebar/>
             <div className={"messenger_contents"}>
                 <header>
@@ -111,9 +163,20 @@ export default function Messenger() {
 
                 <form onSubmit={_onSubmit} className="messenger_form">
                     <input onChange={(e) => setMessage(e.target.value)}
-                           type="text"
                            className={"messenger_form__message"}
-                           placeholder={'Envoyer un message à #général'} value={message}/>
+                           placeholder={'Envoyer un message à #général'} value={message}
+                           title={"Champ d'ecriture du message"}/>
+
+                    <FileInput fileObject={(file) => setTemporyFile(file)}/>
+
+                    <button className={"btn btn-primary mx-3"} title={"Envoyer"}>
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <path
+                                d="M13.7249 7.555L2.72494 2.055C2.63873 2.01189 2.54189 1.99461 2.44608 2.00526C2.35028 2.0159 2.25959 2.05401 2.18494 2.115C2.11365 2.17475 2.06045 2.2532 2.0313 2.34153C2.00215 2.42985 1.99822 2.52456 2.01994 2.615L3.34494 7.5H8.99994V8.5H3.34494L1.99994 13.37C1.97956 13.4455 1.97718 13.5248 1.99299 13.6014C2.00881 13.678 2.04239 13.7499 2.09102 13.8111C2.13965 13.8724 2.20199 13.9214 2.27301 13.9542C2.34403 13.987 2.42176 14.0027 2.49994 14C2.57821 13.9995 2.65528 13.9807 2.72494 13.945L13.7249 8.445C13.8068 8.40304 13.8756 8.3393 13.9236 8.26078C13.9716 8.18226 13.997 8.09203 13.997 8C13.997 7.90798 13.9716 7.81774 13.9236 7.73923C13.8756 7.66071 13.8068 7.59696 13.7249 7.555V7.555Z"
+                                fill="currentColor"/>
+                        </svg>
+                    </button>
                 </form>
             </div>
         </div>
