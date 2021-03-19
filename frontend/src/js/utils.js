952c127f5cf5598d62aaeb9bb718d1451d19e6a8
AlexVWeb <alex-v00@hotmail.fr>
2021-03-19 03:17:11 +0100

    FE: change timing notification

--- frontend/src/js/utils.js
+++ frontend/src/js/utils.js
@@ -34,7 +34,7 @@ export function notification() {
         toast: true,
         position: 'top-end',
         showConfirmButton: false,
-        timer: 3000,
+        timer: 2000,
         timerProgressBar: true,
         didOpen: (toast) => {
             toast.addEventListener('mouseenter', Swal.stopTimer)
